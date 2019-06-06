7f0e241dfbff33f79d7c545ba616722926b6e994
Roberto Marquez <onebeartoe@gmail.com>
2019-06-06 12:43:45 -0500

    ensure the cards' state is consistent after a mismatch

--- memory/cards/game/src/test/java/org/onebeartoe/games/memory/cards/game/play/selection/MemoryCardsGameCardSelectionSpecification.java
+++ memory/cards/game/src/test/java/org/onebeartoe/games/memory/cards/game/play/selection/MemoryCardsGameCardSelectionSpecification.java
@@ -19,23 +19,23 @@ import static org.onebeartoe.games.memory.cards.game.initialization.MenuCardsGam
  *
  * @author Roberto Marquez
  */
-public class MemoryCardsGameCardSelectionSpecification 
+public class MemoryCardsGameCardSelectionSpecification
 {
     private CardsCannedDate cannedData;
 
     @BeforeTest
-    public void setup() 
+    public void setup()
     {
         cannedData = new CardsCannedDate();
     }
 
     /**
      * This class verifies criteria 7
-     * 
+     *
      * Go through 3 rounds verifying guess1 and guess2 are accepted
      */
     @Test
-    public void selection() throws TooFewCardsException, TooManyCardsException, InvalidPairsException, CardsAlreadyInitializedException 
+    public void selection() throws TooFewCardsException, TooManyCardsException, InvalidPairsException, CardsAlreadyInitializedException
     {
         MemoryCardsGame implementation = new MemoryCardsGame();
 
@@ -68,7 +68,7 @@ public class MemoryCardsGameCardSelectionSpecification
      * This class verifies criteria 8
      */
     @Test(expectedExceptions = {IllegalStateException.class})
-    public void selection_failsGuess1() throws TooFewCardsException, InvalidPairsException, TooManyCardsException, CardsAlreadyInitializedException 
+    public void selection_failsGuess1() throws TooFewCardsException, InvalidPairsException, TooManyCardsException, CardsAlreadyInitializedException
     {
         // get past round 1.  in round 2 for guess 1 select a revealed card and expect rejected request
         MemoryCardsGame implementation = new MemoryCardsGame();
@@ -88,13 +88,40 @@ public class MemoryCardsGameCardSelectionSpecification
         MenuCardsGameResponse response3 = implementation.selectCard1();  // again
     }
 
+
+//TODO: implement this test
+def test_selection_failsMismatch(self):
+    print("hellowwowowoow")
+    implementation = MemoryCardsGame();
+    cards = self.cannedData.validCardSetCountOf2();
+    implementation.setCards(cards);
+    implementation.startGame();
+
+    # setup a mismatch
+    response1 = implementation.selectCard1();
+    self.assertEqual(response1, MemoryCardsGameResponse.GUESS_ONE_ACCEPTED);
+    response2 = implementation.selectCard7();
+    self.assertEqual(response2, MemoryCardsGameResponse.GUESS_TWO_ACCEPTED_MISMATCH)
+
+    # verify guess one from the frist round is still selectable
+    response3 = implementation.selectCard1();
+    self.assertEqual(response3, MemoryCardsGameResponse.GUESS_ONE_ACCEPTED);
+
+    response4 = implementation.selectCard6(); # a match for response3
+    self.assertEqual(response4, MemoryCardsGameResponse.GUESS_TWO_ACCEPTED_MATCH);
+
+    # verify guess two from the frist round is still selectable
+    response5 = implementation.selectCard7()
+    self.assertEqual(response5, MemoryCardsGameResponse.GUESS_ONE_ACCEPTED);
+
+
     /**
      * This class verifies criteria 8
      */
     @Test(expectedExceptions = {IllegalStateException.class})
     public void selection_failsGuess2() throws TooFewCardsException, InvalidPairsException, TooManyCardsException, CardsAlreadyInitializedException {
 
-        // get past round 1 and on guess 2 of round 2 select a revealed card and expect rejected request 
+        // get past round 1 and on guess 2 of round 2 select a revealed card and expect rejected request
         MemoryCardsGame implementation = new MemoryCardsGame();
 
         List<MemoryCard> cards = cannedData.validCardSetAllTheSame();
