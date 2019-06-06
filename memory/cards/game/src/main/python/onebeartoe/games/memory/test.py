de2e92966adeb2507ce010fe861075876584cbd2
Roberto Marquez <onebeartoe@gmail.com>
2019-06-05 22:47:23 -0500

    add HTML unit test reports

---
+++ memory/cards/game/src/main/python/onebeartoe/games/memory/test.py
@@ -0,0 +1,25 @@
+
+# install dependencies:
+# $ pip3 install html-testRunner
+
+
+# import dependencies
+from unittest import TestLoader, TestSuite
+from HtmlTestRunner import HTMLTestRunner
+
+from MemoryCardsGameInitializationSpecification import MemoryCardsGameInitializationSpecification
+from MemoryCardsGameCardSelectionSpecification import MemoryCardsGameCardSelectionSpecification
+from MemoryCardsGameEndOfRoundSpecification import MemoryCardsGameEndOfRoundSpecification
+from MemoryCardsGameEndOfGameSpecification import MemoryCardsGameEndOfGameSpecification
+
+# setup unit test suites
+initializationTests = TestLoader().loadTestsFromTestCase(MemoryCardsGameInitializationSpecification)
+selectionTests = TestLoader().loadTestsFromTestCase(MemoryCardsGameCardSelectionSpecification)
+endOfRoundTests = TestLoader().loadTestsFromTestCase(MemoryCardsGameEndOfRoundSpecification)
+endOfGameTests = TestLoader().loadTestsFromTestCase(MemoryCardsGameEndOfGameSpecification)
+
+suite = TestSuite([initializationTests, selectionTests, endOfRoundTests, endOfGameTests])
+
+runner = HTMLTestRunner(add_timestamp=False, combine_reports=True, output='target', report_name="unit-test-report")
+
+runner.run(suite)
