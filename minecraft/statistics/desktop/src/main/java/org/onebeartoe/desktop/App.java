e662ac4bed6e4a0ef5b447cad6ab29a90b07de70
Roberto Marquez <onebeartoe@gmail.com>
2022-01-06 17:26:46 -0600

    whitespace

--- minecraft/statistics/desktop/src/main/java/org/onebeartoe/desktop/App.java
+++ minecraft/statistics/desktop/src/main/java/org/onebeartoe/desktop/App.java
@@ -35,8 +35,8 @@ public class App extends Application
         return fxmlLoader.load();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         launch();
     }
-
 }
