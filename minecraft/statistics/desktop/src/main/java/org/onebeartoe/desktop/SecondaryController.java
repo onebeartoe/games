61f41b0b35a64854ab8815dd5a2850cd5fc49dfd
Roberto Marquez <onebeartoe@gmail.com>
2022-01-15 13:08:57 -0600

    try using AudioClip to play a sound

--- minecraft/statistics/desktop/src/main/java/org/onebeartoe/desktop/SecondaryController.java
+++ minecraft/statistics/desktop/src/main/java/org/onebeartoe/desktop/SecondaryController.java
@@ -1,10 +1,22 @@
+
 package org.onebeartoe.desktop;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.media.AudioClip;
 
 public class SecondaryController 
 {
+    @FXML
+    private void playSound()
+    {
+        System.out.println("playing sound");
+        
+        AudioClip buzzer = new AudioClip(getClass().getResource("/org/onebeartoe/desktop/y2meta.com-Rick-Rolled-Short-Version.mp3").toExternalForm());
+        
+        buzzer.play();
+    }
+    
     @FXML
     private void switchToSplash() throws IOException 
     {
