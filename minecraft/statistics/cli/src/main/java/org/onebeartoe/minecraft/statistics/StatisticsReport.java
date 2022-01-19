e4a48874c6cc0c6d160dac5db84061148621c72b
Roberto Marquez <onebeartoe@gmail.com>
2022-01-18 21:35:12 -0600

    add documentation

--- minecraft/statistics/cli/src/main/java/org/onebeartoe/minecraft/statistics/StatisticsReport.java
+++ minecraft/statistics/cli/src/main/java/org/onebeartoe/minecraft/statistics/StatisticsReport.java
@@ -2,7 +2,7 @@
 package org.onebeartoe.minecraft.statistics;
 
 /**
- *
+ * This domain class represents a single user's report on its Minecraft statistics.
  */
 class StatisticsReport 
 {
---
+++ minecraft/statistics/cli/src/main/java/org/onebeartoe/minecraft/statistics/UserStatisticsService.java
@@ -0,0 +1,17 @@
+
+package org.onebeartoe.minecraft.statistics;
+
+import java.io.File;
+
+/**
+ *
+ */
+public class UserStatisticsService 
+{
+    public StatisticsReport parse(File userStatisticsFile)
+    {
+        
+        
+        return 
+    }
+}
