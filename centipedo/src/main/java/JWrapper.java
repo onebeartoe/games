34501e71eacbef5a832e094de5ed5b426f6fbbee
Roberto Marquez <onebeartoe@gmail.com>
2023-11-03 16:27:51 -0500

    Ctrl-M!

---
+++ centipedo/src/main/java/JWrapper.java
@@ -0,0 +1,42 @@
+/**
+ * The code snippet in this video description helped me remember how to convert a
+ * Java (J)Applet to an 'static void main(args)' application.
+ * 
+ *      https://www.youtube.com/watch?v=CKzzk32UkJA&ab_channel=RollerCoasterManiac
+ * 
+ * The notes at the bottom of this writeup helped remind me of (J)Applet's reliance 
+ * on browser features; getCodeBase(), getAudioClip(), ect...
+ * 
+ * I did this with RandomJuke (Dr Brown's ((I think)) kaleidoscope did this a long time ago.
+ */
+
+
+import javax.swing.JFrame;
+
+//import CubeApp;
+
+
+
+public class Wrapplet 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Rubik World!");
+        
+        JFrame frame = new JFrame("Rubic's Cube - Shiffman Rules!");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+         CubeApp applet = new CubeApp();
+  frame.getContentPane().add(applet);
+  //ADD TO THE CONTENT PANE!!!
+  //i made a mistake
+  applet.init();
+  applet.start();
+  frame.pack();
+  /* Some applets do not allow packing the frame.
+   * Others do, Its mostly trial and error.
+   */
+  frame.setResizable(false);
+  frame.setVisible(true);
+    }
+}
