1e811cce388dc2fb09bf40da41f80167abdf1f10
Roberto Marquez <onebeartoe@gmail.com>
2023-12-28 22:10:45 -0600

    require javafx.base

--- minecraft/statistics/desktop/src/main/java/module-info.java
+++ minecraft/statistics/desktop/src/main/java/module-info.java
@@ -1,9 +1,11 @@
 module org.onebeartoe.desktop {
+    requires javafx.base;
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
     requires javafx.media;
 
     opens org.onebeartoe.desktop to javafx.fxml;
+//    opens org.onebeartoe.desktop to javafx.media;
     exports org.onebeartoe.desktop;
 }
