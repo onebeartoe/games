c3c67947e2be449f22f79ccb04426c2f51f78d28
Roberto Marquez <onebeartoe@gmail.com>
2024-01-02 13:45:48 -0600

    add more integration with the PlayerAdvancementsService

--- minecraft/statistics/desktop/src/main/java/org/onebeartoe/desktop/AdvancementsController.java
+++ minecraft/statistics/desktop/src/main/java/org/onebeartoe/desktop/AdvancementsController.java
@@ -39,10 +39,10 @@ public class AdvancementsController
     TextArea netherHaveNotsTextArea;
     
     @FXML
-    TextArea husbundryHavesTextArea;
+    TextArea husbundryCompleteCatelogueHavesTextArea;
     
     @FXML
-    TextArea husbundryHaveNotsTextArea;
+    TextArea husbundryCompleteCatelogueHaveNotsTextArea;
     
     @FXML
     HBox netherHBox;
@@ -66,19 +66,19 @@ public class AdvancementsController
           
         List<String> allAdventureTimes = implementation.allAdventureTimes();
         
-        husbundryHavesTextArea.setText(allAdventureTimes.toString());
-        
         netherHBox.setSpacing(10);
         
         husbandryHBox.setSpacing(10);
         
-        var advancementsPath = "file:///home/roberto/.minecraft/saves/worldo/advancements/b8da6a01-2a0d-4df1-a86a-94a3e3da6389.json";
+        var advancementsPath = "/home/roberto/.minecraft/saves/worldo/advancements/b8da6a01-2a0d-4df1-a86a-94a3e3da6389.json";
         
-        playerAdvancementsService = new PlayerAdvancementsService();
+        playerAdvancementsService = new PlayerAdvancementsService(advancementsPath);
         
         playerAdvancements = playerAdvancementsService.load(advancementsPath);
         
         showHotTouristDestinationsData();
+        
+        showCompleteCatalogueData();
     }
     
     @FXML
@@ -97,25 +97,47 @@ public class AdvancementsController
         StringBuilder haves = new StringBuilder();        
                 
         playerAdvancements.nether.hotTouristDestinations.haves()
-        .forEach((have) -> 
-        {
-            haves.append(have);
-            haves.append("\n");
-        });
+            .forEach((have) -> 
+            {
+                haves.append(have);
+                haves.append("\n");
+            });
                 
         netherHavesTextArea.setText(haves.toString() );
         
         StringBuilder haveNots = new StringBuilder();
         
         playerAdvancements.nether.hotTouristDestinations.haveNots()
-        .forEach((not) -> 
-        {
-            haveNots.append(not);
-            haveNots.append("\n");
-        });
+            .forEach((not) -> 
+            {
+                haveNots.append(not);
+                haveNots.append("\n");
+            });
         
         netherHaveNotsTextArea.setText(haveNots.toString() );
+    }
+
+    private void showCompleteCatalogueData() 
+    {
+        StringBuilder haves = new StringBuilder();        
+        playerAdvancements.husbandry.aCompleteCatelogue.haves()
+                .forEach((have) ->
+                {
+                    haves.append(have);
+                    haves.append("\n");
+                });
+        
+        StringBuilder nots = new StringBuilder();
+        playerAdvancements.husbandry.aCompleteCatelogue.haveNots()
+                .forEach((not) ->
+                {
+                    nots.append(not);
+                    nots.append("\n");
+                });
         
         
+        husbundryCompleteCatelogueHavesTextArea.setText(haves.toString());
+        
+        husbundryCompleteCatelogueHaveNotsTextArea.setText(nots.toString());
     }
 }
