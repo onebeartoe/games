c675eda16126e2ba430b17751f0f7df9a4fd4bb6
Roberto Marquez <onebeartoe@gmail.com>
2024-01-09 15:58:39 -0600

    set the width of the "Back to Game" button

--- minecraft/advancements/desktop/src/main/java/org/onebeartoe/desktop/GameMenuController.java
+++ minecraft/advancements/desktop/src/main/java/org/onebeartoe/desktop/GameMenuController.java
@@ -2,6 +2,7 @@ package org.onebeartoe.desktop;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
@@ -23,9 +24,24 @@ public class GameMenuController
     Button singlePlayerButton;
     
     @FXML
+    Button backButton;
+    
+    @FXML
     public void initialize() throws URISyntaxException, IOException
     {
+//        String css = "-fx-border-color: red;\n" +
+//                   "-fx-border-insets: 5;\n" +
+//                   "-fx-border-width: 3;\n" +
+//                   "-fx-border-style: dashed;\n";
+//        
+//        vBox.setStyle(css); 
+    
+//    backButton.prefWidthProperty().bind(Bindings.divide(vBox.widthProperty(), 0.01));
+    backButton.prefWidthProperty().bind( vBox.widthProperty().multiply(0.4) );
+//    backButton.prefWidthProperty().bind(Bindings.divide(primaryStage.widthProperty(), 10.0));
 
+
+System.out.println("game menu initialized");        
     }
     
     @FXML
