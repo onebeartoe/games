664c5d14caac1bec33426184465ddb28294b9471
Roberto Marquez <onebeartoe@gmail.com>
2024-01-10 12:49:01 -0600

    cleanup

--- minecraft/advancements/desktop/src/test/java/org/onebeartoe/desktop/AdvancementsControllerTest.java
+++ minecraft/advancements/desktop/src/test/java/org/onebeartoe/desktop/AdvancementsControllerTest.java
@@ -1,12 +1,7 @@
 
 package org.onebeartoe.desktop;
 
-import static java.lang.ProcessBuilder.Redirect.from;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 import org.testfx.framework.junit.ApplicationTest;
-import static org.testng.AssertJUnit.assertEquals;
 import org.testng.annotations.Test;
 
 /**
@@ -22,21 +17,5 @@ public class AdvancementsControllerTest extends ApplicationTest
         System.out.println("this is a test - 12345431 - kjljl;kjlkjl;j");
     }
 
-//  @Override
-//  public void start(Stage stage) throws Exception {
-//    stage.setScene(helloWorld.getScene());
-//    stage.show();
-//    stage.toFront();
-//  }
-//  @Test
-//  public void hasHelloWorldButton() {
-//    StackPane rootNode = helloWorld.getScene().getRoot();
-//    Button button = from(rootNode).lookup(".button").query();
-//    assertEquals("Hello World", button.getText());
-//  }
 }
 
-//public class HelloWorldTest  {
-//  private final HelloWorld helloWorld = new HelloWorld();
-//
-//}
