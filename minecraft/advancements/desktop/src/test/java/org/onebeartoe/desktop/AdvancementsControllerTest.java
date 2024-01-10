9409a27171af451e1af3bdc92fcab7fb9164df6b
Roberto Marquez <onebeartoe@gmail.com>
2024-01-10 12:39:39 -0600

    add JavaFX tests via the org.testfx dependency

--- minecraft/advancements/desktop/src/test/java/org/onebeartoe/desktop/AdvancementsControllerTest.java
+++ minecraft/advancements/desktop/src/test/java/org/onebeartoe/desktop/AdvancementsControllerTest.java
@@ -1,12 +1,18 @@
 
 package org.onebeartoe.desktop;
 
+import static java.lang.ProcessBuilder.Redirect.from;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import org.testfx.framework.junit.ApplicationTest;
+import static org.testng.AssertJUnit.assertEquals;
 import org.testng.annotations.Test;
 
 /**
  *
  */
-public class AdvancementsControllerTest
+public class AdvancementsControllerTest extends ApplicationTest
 {
     private AdvancementsController implementation = new AdvancementsController();
     
@@ -15,4 +21,22 @@ public class AdvancementsControllerTest
     {
         System.out.println("this is a test - 12345431 - kjljl;kjlkjl;j");
     }
+
+//  @Override
+//  public void start(Stage stage) throws Exception {
+//    stage.setScene(helloWorld.getScene());
+//    stage.show();
+//    stage.toFront();
+//  }
+//  @Test
+//  public void hasHelloWorldButton() {
+//    StackPane rootNode = helloWorld.getScene().getRoot();
+//    Button button = from(rootNode).lookup(".button").query();
+//    assertEquals("Hello World", button.getText());
+//  }
 }
+
+//public class HelloWorldTest  {
+//  private final HelloWorld helloWorld = new HelloWorld();
+//
+//}
