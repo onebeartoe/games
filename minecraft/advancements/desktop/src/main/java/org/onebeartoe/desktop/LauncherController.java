acc1d29632aa6be1de38762b6286a6f130a9eb92
Roberto Marquez <onebeartoe@gmail.com>
2024-01-27 07:56:47 -0600

    cleanup

--- minecraft/advancements/desktop/src/main/java/org/onebeartoe/desktop/LauncherController.java
+++ minecraft/advancements/desktop/src/main/java/org/onebeartoe/desktop/LauncherController.java
@@ -4,13 +4,7 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 
 public class LauncherController 
-{
-    @FXML
-    private void switchToSecondary() throws IOException
-    {
-        App.setRoot("secondary");
-    }
-    
+{    
     @FXML
     private void handlePlayButton() throws IOException
     {
