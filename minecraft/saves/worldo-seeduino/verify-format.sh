d02ba34ee1e38958fd277c7fb7623c1e16af6e4c
Roberto Marquez <onebeartoe@gmail.com>
2024-02-13 18:00:32 -0600

    specify a single file

--- minecraft/saves/worldo-seeduino/verify-format.sh
+++ minecraft/saves/worldo-seeduino/verify-format.sh
@@ -2,4 +2,5 @@
 java --source 21 \
      --enable-preview \
      -DrequireFourItems=true \
-     ../dragon-fart-2000/maps/watcher/GnuplotDataVerification.java
+     ../dragon-fart-2000/maps/watcher/GnuplotDataVerification.java \
+     nether.data
