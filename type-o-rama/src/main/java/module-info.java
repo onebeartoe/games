c40681ade03c7a1929553258fdabc3ae0afc400b
Roberto Marquez <onebeartoe@gmail.com>
2024-05-06 08:39:59 -0500

    require the javafx.media module

--- type-o-rama/src/main/java/module-info.java
+++ type-o-rama/src/main/java/module-info.java
@@ -1,6 +1,8 @@
-module org.onebeartoe.games.type.o.rama {
+module org.onebeartoe.games.type.o.rama
+{
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
 
     opens org.onebeartoe.games.type.o.rama to javafx.fxml;
     exports org.onebeartoe.games.type.o.rama;
